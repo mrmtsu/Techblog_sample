6d42024014c9ef9ff461e74be3637860e5be955c
mrmtsu <81202muramatsu@gmail.com>
2020-04-11 10:00:46 +0900

    Initial commit

---
+++ database/migrations/2020_04_11_005719_create_todos_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('todo');     // この行を追加
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
